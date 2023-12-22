d233c506d9f3adf2ac80d8bcffab87974414843a
DarekDe <darek3@hotmail.com>
2023-12-22 17:28:15 +0100

    usuwam folder zawierający 'python' i pakuję wszytko bezpośrednio do repo

---
+++ 2.py
@@ -0,0 +1,11 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Allice')
+elif age < 12:
+    print('You are not Allice, kiddo!')
+elif age > 2000:
+    print('Unlike you immortal vampire!')
+elif age > 100:
+    print('You are not Allice grannie.')
+    

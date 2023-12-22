2c1a15076fd3d643258b774af981844108c6b9ab
DarekDe <darek3@hotmail.com>
2023-12-22 23:44:12 +0100

    uproszczenie 18-tki

--- 18_regular_expression_2.py
+++ 18_regular_expression_2.py
@@ -3,5 +3,4 @@ import re
 message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999'
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.search(message)
-print(mo.group())
+print(phoneNumRegex.findall(message))

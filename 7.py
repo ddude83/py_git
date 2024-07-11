4fa70f387f7a0dff29ec9b1d2a48fa644bf53970
Darko <darek3@hotmail.com>
2024-07-11 15:58:46 +0200

    Rozdzielam kodillę od Automete

--- 7.py
+++
@@ -1,3 +0,0 @@
-print('Hej Darku jak masz na imię?')
-for i in range(10):
-    print('Mam na imię Darek i powiem to po raz ' + str(i))
---
+++ automateTheBorringStuff/10_functions.py
@@ -0,0 +1,4 @@
+def hello(name, wishes):
+    print('Siema panie ' + name + ', nie przejmuj się, przejdzie jej. Jak zwykle z resztą. Życzę Ci ' + wishes + '.')
+    
+hello('Darek', 'wszystkiego najlepszego')
